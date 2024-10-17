db6cd9c231f39a5cbc732cf5d675ab77059de4e1
EMMIR <109979235+LMP88959@users.noreply.github.com>
2024-10-17 01:04:53 -0500

    Add do_dark_intra_boost option

--- src/dsv_encoder.h
+++ src/dsv_encoder.h
@@ -74,6 +74,7 @@ typedef struct {
     int do_scd; /* scene change detection */
     int do_temporal_aq; /* toggle temporal adaptive quantization for I frames */
     int do_psy; /* enable psychovisual optimizations */
+    int do_dark_intra_boost; /* boost quality in dark intra frames */
 
     /* threshold for skip block determination. -1 = disable
     Larger value = more likely to mark it as skipped */
