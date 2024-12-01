60cfd3dd35acca8a0edabe5992dfb3d585d03475
EMMIR <109979235+LMP88959@users.noreply.github.com>
2024-11-30 23:05:06 -0600

    Add complexity tracking data members

--- src/dsv_encoder.h
+++ src/dsv_encoder.h
@@ -110,6 +110,8 @@ typedef struct {
     int bpf_avg;
     int total_P_frame_q;
     int avg_P_frame_q;
+    int prev_complexity;
+    int curr_complexity;
 
     DSV_FNUM next_fnum;
     DSV_ENCDATA *ref;
